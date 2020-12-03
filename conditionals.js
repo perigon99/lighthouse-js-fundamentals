2a6dff2e10f0b8faaedc02a063f411d80bd3ac45
Sebastien Perot-Morin <perigon@hotmail.com>
2020-12-03 17:33:22 +0000

    Add more conditionals

--- conditionals.js
+++ conditionals.js
@@ -24,3 +24,24 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+//code no 3
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+//code no 4
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+//code no 5
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
